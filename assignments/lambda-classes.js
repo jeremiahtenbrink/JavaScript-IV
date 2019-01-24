2b031a06255d483ecc8465becead7c33ded4aade
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-01-24 10:42:42 -0700

    Started the assignment.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,37 @@
 // CODE here for your Lambda Classes
+class Person {
+	constructor({ name, age, location, gender }){
+		this.name = name;
+		this.age = age;
+		this.location = location;
+		this.gender = gender;
+	}
+	
+	speak = function () {
+		console.log(`Hello, my name is ${this.name}, I am from ${this.location}`)
+	}
+}
+
+class Instructor extends Person {
+	constructor( attributes ) {
+		super(attributes);
+		this.specialty = attributes.specialty;
+		this.favLanguage = attributes.favLanguage;
+		this.catchPhrase = attributes.catchPhrase;
+	}
+	
+	deamo = function (subject) {
+		console.log(`Today we are learning about ${subject}`);
+	};
+	
+	grade = function (student, subject) {
+			console.log(`${student.name} receives a perfect score on ${subject}.`)
+	};
+}
+
+class Student extends Person {
+	constructor(attributes) {
+		super(attributes);
+		
+	}
+}
